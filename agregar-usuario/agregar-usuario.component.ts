a34fbe131e2c89a2185c188140090c578ff31503
Mariana Urquidy Galaviz <134175927+1234mmmm34@users.noreply.github.com>
2024-05-15 13:59:21 -0700

    Primer commit

---
+++ agregar-usuario/agregar-usuario.component.ts
@@ -0,0 +1,335 @@
+import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { DataService } from '../data.service';
+import { usuario, usuarios } from "../modelos/usuarios"
+import { fraccionamiento, fraccionamientos } from "../modelos/fraccionamientos"
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
+import Swal from 'sweetalert2'
+import { v4 as uuidv4 } from 'uuid';
+import { InvitacionService } from './invitacion.service';
+import { CorreoService } from './correo.service';
+import { Usuario } from '../invitacion/usuario.model';
+
+ 
+@Component({
+  selector: 'app-agregar-usuario',
+  templateUrl: './agregar-usuario.component.html',
+  styleUrls: ['./agregar-usuario.component.css']
+})
+export class AgregarUsuarioComponent {
+  usuario = new usuario();
+  usuarios: usuarios[] = [];
+  UserGroup: FormGroup;
+  fraccionamientos: fraccionamientos[] | undefined;
+  id_fracc: any;
+  tipo_usuario: any;
+  filtroUsuarios: "" | undefined;
+  id_usuario: any;
+
+indice: number = 0; 
+verdaderoRango: number = 6;
+cont: number = 1;
+usuarios1: usuarios[] = [];
+
+  ngOnInit(): void {
+
+    this.fetchDataUsers(this.dataService.obtener_usuario(1));
+
+
+    $(function () {
+
+      // Active the first thumb & panel
+      $(".tabs-thumb1:first-child").addClass("is-active").closest(".tabs1").find(".tabs-panel1:first-child").show();
+
+      $(this).addClass("is-active");
+
+      $(".tabs-thumb1").click(function () {
+        // Cancel the siblings
+        $(this).siblings(".tabs-thumb1").removeClass("is-active").closest(".tabs1").find(".tabs-panel1").hide();
+        $(this).addClass("is-active");
+        // Active the thumb & panel
+        $(this).addClass("is-active1").closest(".tabs1").find(".tabs-panel1").eq($(this).index(".tabs-thumb1")).show();
+      });
+    });
+
+
+  
+  }
+
+  fetchDataUsers(id_administrador: any) {
+    console.log("id admiiiiiiiiiiiiiiiiiiiiiiin: " + id_administrador);
+    this.dataService.fetchDataUsers(id_administrador).subscribe((usuarios: usuarios[]) => {
+      console.log("fetch", usuarios);
+      this.usuarios = usuarios;
+    });
+  }
+
+
+  paginador_atras() {
+
+    if (this.indice - this.verdaderoRango >= 0) {
+      this.usuarios1 = this.usuarios.slice(this.indice - this.verdaderoRango, this.indice);
+      this.indice = this.indice - this.verdaderoRango;
+      this.cont--;
+    }
+  }
+
+  paginador_adelante() {
+    if (this.usuarios.length - (this.indice + this.verdaderoRango) > 0) {
+      this.indice = this.indice + this.verdaderoRango;
+      this.usuarios1 = this.usuarios.slice(this.indice, this.indice + this.verdaderoRango);
+      this.cont++;
+     // this.consultarNotificacion
+    } 
+  }
+
+  correo_invitado: string = '';
+  correo_invitado1: string = '';
+
+  seleccionar(usuario: any){
+    this.id_usuario = usuario;
+  }
+
+
+  enviarCorreo(correoDestinatario: string, mensaje: string): void {
+    this.correoService.Enviar_Correo(correoDestinatario, this.dataService.obtener_usuario(5) + " te ha invitado a unirte a su comunidad, da click en el enlace y llena el formulario.");
+  }
+
+
+
+  mandar_correo(correo_invitado: string) {
+
+
+    console.log("generarinvitacion");
+    const token = uuidv4();
+    console.log(token, correo_invitado, this.dataService.obtener_usuario(1), this.dataService.obtener_usuario(5), this.dataService.obtener_usuario(2), "usuario")
+
+    var correo = correo_invitado;
+    this.invitacionService.generarInvitacion(token, "urquidymariana@gmail.com", this.dataService.obtener_usuario(1), this.dataService.obtener_usuario(5), this.dataService.obtener_usuario(2), "usuario")
+      .subscribe(
+        response => {
+          console.log('Success:', response);
+          this.correoService.Enviar_Correo("urquidymariana@gmail.com", "haz sido invitado por tu administrador para unirte a una comunidad en linea\n por favor termina tu registro en el siguiente link: \n http://localhost:4200/Invitacion?token=" + token);
+          // this.enviarCorreo(correoElectronico, "haz sido invitado por tu administrador para unirte a una comunidad en linea\n por favor termina tu registro en el siguiente link: \n http://localhost:4200/Invitacion?token=" + token);
+          Swal.fire({
+            title: 'Invitacion enviada correctamente',
+            text: '',
+            icon: 'success',
+            confirmButtonText: 'Aceptar'
+          })
+
+        },
+        error => {
+          console.error('Error al generar la invitación:', error);
+
+        }
+      )
+  };
+
+
+
+
+  agregar_usuario(usuario: {
+
+    id_usuario: number | undefined;
+    nombre: string | undefined;
+    apellido_pat: string | undefined;
+    apellido_mat: string | undefined;
+    id_fraccionamiento: number | undefined;
+    tipo_usuario: string | undefined;
+    id_lote: number | undefined;
+    telefono: string | undefined;
+    fecha_nacimiento: Date | undefined;
+    correo: string | undefined;
+    contrasenia: string | undefined;
+    confirmarContrasena: string | undefined;
+
+  }) {
+
+    if (usuario.contrasenia == usuario.confirmarContrasena) {
+
+      const params = {
+        nombre: usuario.nombre,
+        apellido_pat: usuario.apellido_pat,
+        apellido_mat: usuario.apellido_mat,
+        tipo_usuario: "usuario",
+        telefono: usuario.telefono,
+        fecha_nacimiento: usuario.fecha_nacimiento,
+        correo: "N/A",
+        contrasenia: usuario.contrasenia,
+        id_fraccionamiento: this.dataService.obtener_usuario(1),
+        id_administrador: this.dataService.obtener_usuario(1),
+        id_lote: 1,
+        hikvision: "permitido"
+
+        //  Intercomunicador: 123,
+        //  Codigo_acceso: "123"
+      };
+
+      let direccion = "https://localhost:44397/api/Usuarios/Agregar_Usuario";
+
+      const headers = new HttpHeaders({ 'myHeader': 'procademy' });
+      this.http.post(
+        direccion,
+        params, { headers: headers })
+        .subscribe((res) => {
+          console.log(res);
+          console.log(this.usuarios[0].fecha_nacimiento); 
+          this.fetchDataUsers(this.dataService.obtener_usuario(1));
+
+
+        });
+    }
+    else {
+      console.log("error: intentalo de nuevo");
+    }
+
+    this.usuarios.push(this.UserGroup.value);
+    this.UserGroup.reset();
+
+  }
+
+
+  disable(usuario: any) {
+    
+    this.id_fracc = this.id_usuario;
+
+    return this.http.get("https://localhost:44397/api/Usuario_lote/RestrictedUser?id_usuario=" + this.id_fracc).subscribe(
+      () => {
+
+        Swal.fire({
+          title: 'Usuario restringido correctamente',
+          text: '',
+          icon: 'success',
+          confirmButtonText: 'Aceptar'
+        })
+        this.UserGroup.reset();
+        this.fetchDataUsers(this.dataService.obtener_usuario(1));
+
+      },
+      (error) => {
+        console.error('Error al agregar notificación Angular:', error);
+        Swal.fire({
+          title: 'Por favor, complete todos los campos',
+          text: '',
+          icon: 'error',
+          confirmButtonText: 'Aceptar'
+        })
+        this.fetchDataUsers(this.dataService.obtener_usuario(1));
+        console.log("hola");
+        this.UserGroup.reset();
+
+
+      })
+
+
+  }
+
+
+  delete(usuario: any) {
+    
+    this.id_fracc = this.id_usuario;
+
+    return this.http.delete("https://localhost:44397/api/Usuario_lote/Eliminar_inquilino?id_usuario=" + this.id_fracc).subscribe(
+      () => {
+
+        Swal.fire({
+          title: 'Usuario eliminado correctamente',
+          text: '',
+          icon: 'success',
+          confirmButtonText: 'Aceptar'
+        })
+        this.UserGroup.reset();
+        this.fetchDataUsers(this.dataService.obtener_usuario(1));
+
+      },
+      (error) => {
+        console.error('Error al agregar notificación Angular:', error);
+        Swal.fire({
+          title: 'Por favor, complete todos los campos',
+          text: '',
+          icon: 'error',
+          confirmButtonText: 'Aceptar'
+        })
+        this.fetchDataUsers(this.dataService.obtener_usuario(1));
+        console.log("hola");
+        this.UserGroup.reset();
+
+
+      })
+
+
+  }
+ 
+
+  enable(usuario: any) {
+
+    this.id_fracc = this.id_usuario;
+
+    
+    return this.http.get("https://localhost:44397/api/Usuario_lote/EnableUser?id_usuario=" + this.id_fracc).subscribe(
+      () => {
+        Swal.fire({
+          title: 'Usuario agregado correctamente',
+          text: '',
+          icon: 'success',
+          confirmButtonText: 'Aceptar'
+        })
+        this.fetchDataUsers(this.dataService.obtener_usuario(1));
+
+      },
+      (error) => {
+        console.error('Error al agregar notificación Angular:', error);
+        Swal.fire({
+          title: 'Por favor, complete todos los campos',
+          text: '',
+          icon: 'error',
+          confirmButtonText: 'Aceptar'
+        })
+        this.fetchDataUsers(this.dataService.obtener_usuario(1));
+        console.log("hola");
+        this.UserGroup.reset();
+
+
+      })
+
+
+  }
+
+
+
+  constructor(private http: HttpClient, private dataService: DataService, private fb: FormBuilder, private invitacionService: InvitacionService, private correoService: CorreoService) {
+
+    this.UserGroup = this.fb.group({
+      id_persona: ['', Validators.required],
+      nombre: ['', Validators.required],
+      apellido_pat: ['', Validators.required],
+      apellido_mat: ['', Validators.required],
+      tipo_usuario: ['', Validators.required],
+      id_lote: ['', Validators.required],
+      telefono: ['', Validators.required],
+      fecha_nacimiento: ['', Validators.required],
+      correo: ['', Validators.required],
+      contrasenia: ['', Validators.required],
+      confirmarContrasena: ['', Validators.required],
+      id_fraccionamiento: ['', Validators.required],
+
+    })
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ consultar-acuerdos/consultar-acuerdos.component.ts
@@ -0,0 +1,153 @@
+import { Component, ViewChild } from '@angular/core';
+import { AcuerdosService } from '../acuerdos/acuerdos.service';
+import { Acuerdos } from '../acuerdos/acuerdos.model';
+import { DataService } from '../data.service';
+import { NoEncontradoDirective } from '../no-encontrado/no-encontrado.directive';
+import Swal from 'sweetalert2'
+import { NgForm } from '@angular/forms';
+ 
+@Component({
+  selector: 'app-consultar-acuerdos',
+  templateUrl: './consultar-acuerdos.component.html',
+  styleUrls: ['./consultar-acuerdos.component.css'],
+ // directives: [NoEncontradoDirective] // Asegúrate de incluir la directiva en la lista de directivas
+
+})
+export class ConsultarAcuerdosComponent { 
+@ViewChild('formCrear', { static: false }) formCrear!: NgForm;
+//OJOOOOOOOOOOOOOOO
+id_fraccionamiento:number=this.dataService.obtener_usuario(1);
+filtroAcuerdos:'' | undefined;
+filtroFecha:'' | undefined;
+//OJOOOOOOOOOOOOO
+fecha: string=""; 
+//fecha: any;
+acuerdos: Acuerdos[] = [];
+indice: number = 0; 
+verdaderoRango: number = 6;
+cont: number = 1;
+acuerdos1: Acuerdos[] = [];
+
+constructor(private acuerdosService: AcuerdosService,private dataService:DataService) {}
+
+ngOnInit(): void {
+  //this.consultarAcuerdos();
+  this.consultarAcuerdos();
+  
+}
+  
+
+
+
+ 
+
+
+agregarAcuerdo(formulario: any) {
+  //const { idFraccionamiento, asunto, detalles } = formulario;
+  this.fecha = this.obtenerFechaActualFormateada();
+  const asunto = formulario.asunto;
+  const detalles = formulario.detalles;
+
+  console.log("veamos: ",asunto, detalles,this.fecha) 
+  this.acuerdosService.agregarAcuerdo(this.dataService.obtener_usuario(1), asunto, detalles,this.fecha)
+    .subscribe(
+      (respuesta: string) => {
+        Swal.fire({
+          title: 'Acuerdo agregado correctamente',
+          text: '',
+          icon: 'success',
+          confirmButtonText: 'Aceptar' 
+        })
+      //  this..reset(); 
+       // this.formCrear.resetForm();
+        console.log('Acuerdo agregado:', respuesta);
+        this.ngOnInit();
+        //location.reload();
+      },
+      (error) => {
+        Swal.fire({
+          title: 'Por favor, complete todos los campos',
+          text: '',
+          icon: 'error',
+          confirmButtonText: 'Aceptar'
+        })
+        console.error('Error al agregar acuerdo:', error);
+      }
+    );
+}
+
+
+
+
+
+  paginador_atras() {
+
+    if (this.indice - this.verdaderoRango >= 0) {
+      this.acuerdos1 = this.acuerdos.slice(this.indice - this.verdaderoRango, this.indice);
+      this.indice = this.indice - this.verdaderoRango;
+      this.cont--;
+    }
+  }
+
+  paginador_adelante() {
+    if (this.acuerdos.length - (this.indice + this.verdaderoRango) > 0) {
+      this.indice = this.indice + this.verdaderoRango;
+      this.acuerdos1 = this.acuerdos.slice(this.indice, this.indice + this.verdaderoRango);
+      this.cont++;
+     // this.consultarNotificacion
+    } 
+    
+  }
+
+
+  consultarAcuerdos(): void {
+    const idFraccionamiento = this.dataService.obtener_usuario(1); 
+   
+    this.acuerdosService.consultarAcuerdosPaginados(idFraccionamiento, 0, 100).subscribe(
+      (data: Acuerdos[]) => {
+        
+        if(data.length!=0){ 
+          
+          this.acuerdos = data;
+          this.acuerdos1 = this.acuerdos.slice(this.indice, this.indice + this.verdaderoRango);
+       
+          
+        }
+      });
+  }
+
+
+  eliminarAcuerdo(id_acuerdo: number,id_fraccionamiento: number): void {
+    const idFraccionamiento = this.dataService.obtener_usuario(1); // Reemplaza con el ID de fraccionamiento correspondiente
+    this.acuerdosService.eliminarAcuerdo(id_acuerdo,id_fraccionamiento).subscribe(
+      (respuesta: any) => {
+        console.log('Acuerdo eliminado:', respuesta);
+        // Vuelve a cargar los acuerdos después de eliminar
+        this.consultarAcuerdos();
+      },
+      (error) => {
+        console.error('Error al eliminar acuerdo:', error);
+        // Manejo de errores
+      }
+    );
+  }
+
+
+  obtenerFechaActualFormateada(): string {
+    const today = new Date();
+    const year = today.getFullYear();
+    let month: string | number = today.getMonth() + 1; // Los meses van de 0 a 11, sumamos 1 para obtener el mes actual
+    let day: string | number = today.getDate();
+  
+    // Aseguramos que el mes y el día tengan dos dígitos
+    month = month < 10 ? '0' + month : month;
+    day = day < 10 ? '0' + day : day;
+  
+   // console.log(`${year}-${month}-${day}`)
+    return `${year}-${month}-${day}`;
+  // return `${day}-${month}-${year}`;
+  }
+  
+
+
+}
